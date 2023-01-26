cb382d413f27341280fce189ec41232ee9fe0e4f
opmcvs <opmcvs@gmail.com>
2023-01-27 02:57:53 +0800

    For Jan 26 23

--- test1/01tut/src/Content.js
+++ test1/01tut/src/Content.js
@@ -1,32 +1,82 @@
 import React from 'react'
+import {useState} from 'react';
+import{FaTrashAlt} from 'react-icons/fa';
 
 const Content = () => {
-      const handleNameChange = ()=>{
-    const names = ['bob','weird','dave'];
-    const int = Math.floor(Math.random()*3) ;
-    return names[int];
-      }
+      const [items, setItems] = useState([
 
-      const handleClick = ()=>{
-        console.log('You clicked it')
-      }
+        {
+          id:1,
+         checked:false,
+         item: 'One half of Secret' 
+          
+        },
+        {
+          id:2,
+         checked:false,
+         item: 'item2' 
+          },
 
-       const handleClick2 = (name)=>{
-        console.log(`${name} was clicked`)
-      }
+        {
+          id:3,
+         checked:false,
+         item: 'item3' 
+          
+        }
+      ]);
+          // getter, setter           // default state inside the parameter
+      // const [count,setCount] = useState(0);    
+      // const handleNameChange = ()=>{
+    // const names = ['bob','weird','dave'];
+    // const int = Math.floor(Math.random()*3) ;
+    // setName( names[int]);
+    //   }
+
+    //   const handleClick = ()=>{
+    //     setCount(count + 1)
+    //     // the value of count still remains 0 as originally set
+    //     //  it won't change rapidly?
+    //     console.log(count)
+    //   }
+
+    //    const handleClick2 = (name)=>{
+    //     console.log(count)
+    //   }
  
-      const handleClick3 = (e)=>{
-        console.log(e.target.innerText)
-      }
+    //   const handleClick3 = (e)=>{
+    //     console.log(e.target.innerText)
+    //   }
   return (
     <main>
-      <p onDoubleClick={handleClick}>
-        Hello there {handleNameChange()} !
+      {/* <p onDoubleClick={handleClick}>
+        Hello there {name()} !
       </p> 
+      <button onClick={handleNameChange}>change Name</button>  
       <button onClick={handleClick}>click it</button>  
-      <button onClick={()=> handleClick2('Om')}>click it</button>  
-   {/* above is an anomimous function ()=>{} but no need the brackets  */}
-       <button onClick={(e)=> handleClick3(e)}>click it</button> 
+   {/* above is an anomimous function ()=>{} but no need the brackets
+   as only one line using arrow function   */}
+       {/* <button onClick={(e)=> handleClick3(e)}>click it</button>  */}
+       {/* <button onClick={handleClick2}>click it</button> */ }
+
+     <ul>
+      {items.map((item) => {
+        <li className ='item' key={item.id}>
+          <input
+          type='checkbox'
+          checked ={item.checked}/>
+          <label>{item.item}</label>
+          <FaTrashAlt 
+          role='button' 
+          tabIndex='0'/>
+          
+                {/* each list item needs a key in react */}
+        </li>
+      }
+      )}
+
+     </ul>
+
+
     </main>
   )
 }
